0bcf39e544f8a86e66d95f0c785b07993dd81020
lennyroy <lennyroyrobles@gmail.com>
2020-01-20 15:42:48 -0500

    edit meta information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'The Basics: Intro to Web Development',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Learn the basics of HTMl & CSS with Lennyroy. In this 8 week course we will dive deep into the foundations of modern web technology.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
